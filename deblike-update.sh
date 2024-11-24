06628b86aa0d37ae14cbe681f9fd90e9d99c12bc
enigma158an201 <gwenael.goujon@gmail.com>
2024-11-24 12:47:52 +0100

    added git auto update deblike-update.sh

--- deblike-update.sh
+++ deblike-update.sh
@@ -18,6 +18,16 @@ clean_dpkg() {
 	else 							apt-get autoremove --purge "$(dpkg -l | grep ^rc | awk '{print $2}')"
 	fi
 }
+updateScriptsViaGit(){
+	set +euo pipefail #in case find cannot access some files or folders
+	sTargetScript="$(find ~ -type f -iname git-pull-refresh.sh 2>/dev/null)" # -exec {} \;
+	set -euo pipefail
+	if test -f "${sTargetScript}"; then 
+		sGitFolder="$(dirname "${sTargetScript}")"
+		cd "${sGitFolder}" || exit 1
+		bash -x "${sTargetScript}"
+	fi
+}
 main_deblike_update() {
 	if ! command -v apt-get || ! command -v apt; then 
 		echo -e "\t>>> apt not found, exit now !!!"
@@ -25,6 +35,7 @@ main_deblike_update() {
 	else
 		echo -e "\t>>> apt found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4.shutdown vm"
 	fi
+	updateScriptsViaGit
 	update_apt && clean_apt && clean_dpkg && shutdown 0
 }
 
