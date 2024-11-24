f6bb28958412990d370be78211302310dd6fac5e
enigma158an201 <gwenael.goujon@gmail.com>
2024-11-24 10:41:14 +0100

    create git-pull-refresh.sh

---
+++ git-pull-refresh.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# script by enigma158an201
+set -euo pipefail # set -euxo pipefail
+
+findScriptsGitFolder() {
+	find ~ -type d -iname vm-guests-script
+}
+
+checkGitUpdates() {
+	git pull --dry-run | grep -q -v -E 'Already up-to-date.|Déjà à jour.' && return 0 || return 1 #changed=1
+}
+
+pullGitUpdates() {
+	git pull
+}
+
+main_git_process() {
+	if ! command -v git; then 
+		echo -e "\t>>> git not found, exit now !!!"
+		exit 1
+	else
+		echo -e "\t>>> git found, this script will: check and fetch git updates if needed"
+	fi
+	sGitFolder=$(findScriptsGitFolder)
+	cd "${sGitFolder}"
+	checkGitUpdates || pullGitUpdates
+}
+
+main_git_process
