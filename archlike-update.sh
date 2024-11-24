48a90dffb3e841214f4c29a97e43bf97e5e4ad29
enigma158an201 <gwenael.goujon@gmail.com>
2024-11-24 10:45:57 +0100

    add git update to archlike-update.sh

--- archlike-update.sh
+++ archlike-update.sh
@@ -4,39 +4,43 @@
 set -euo pipefail # set -euxo pipefail 
 
 update_arch() {
-    sudo pacman -Syyuu
+	sudo pacman -Syyuu
 }
 clean_arch() {
-    sudo pacman -Scc
-    clean_trizen
-    clean_paru
+	sudo pacman -Scc
+	clean_trizen
+	clean_paru
 }
 clean_paru() {
-    if command -v paru &>/dev/null; then 
-        paru -Sccd
-    else
-        setup_paru
-    fi
+	if command -v paru &>/dev/null; then 
+		paru -Sccd
+	else
+		setup_paru
+	fi
 }
 clean_trizen() {
-    if command -v trizen &>/dev/null; then trizen -Sccd; fi
+	if command -v trizen &>/dev/null; then trizen -Sccd; fi
 }
 setup_paru() {
-    cd /tmp
-    sudo pacman -S --needed base-devel
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si
+	cd /tmp
+	sudo pacman -S --needed base-devel
+	git clone https://aur.archlinux.org/paru.git
+	cd paru
+	makepkg -si
+}
+updateScriptsViaGit(){
+	./git-pull-refresh.sh
 }
 
 main_archlike_update() {
-    if ! command -v pacman; then 
-	    echo -e "\t>>> pacman not found, exit now !!!"
-        exit 1
-    else
-        echo -e "\t>>> pacman found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4.shutdown vm"
-    fi
-    update_arch && clean_arch && shutdown 0
+	if ! command -v pacman; then 
+		echo -e "\t>>> pacman not found, exit now !!!"
+		exit 1
+	else
+		echo -e "\t>>> pacman found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4.shutdown vm"
+	fi
+	updateScriptsViaGit
+	update_arch && clean_arch && shutdown 0
 }
 
 main_archlike_update
