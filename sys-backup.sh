8d6f8b0d3befe8641fc01bd72aaebd78f2224e9e
enigma158an201 <gwenael.goujon@gmail.com>
2024-12-18 02:50:00 +0100

    fix sys-backup.sh

--- sys-backup.sh
+++ sys-backup.sh
@@ -5,14 +5,14 @@ set -euo pipefail # set -euxo pipefail
 
 sBackupHost=gwen@192.168.0.53
 getBackupFilename() {
-	if command -v hostname; then sHostName=$(hostname); else exit 1; fi
+	if command -v hostname &>dev/null; then sHostName=$(hostname); else exit 1; fi
 	sBackupFile="ssh_backup_${sHostName}_$(date +%Y-%m-%d).tar.gz"
 	echo "${sBackupFile}"
 }
 sBackupFile="$(getBackupFilename)"
 
 main_sys_bakcup() {
-    if ! command -v tar  &>/dev/null || ! command -v ssh &>/dev/null; then
+    if ! command -v tar &>/dev/null || ! command -v ssh &>/dev/null; then
         echo -e "\t>>> tar or ssh not found, exit now !!!"
     fi
     cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
