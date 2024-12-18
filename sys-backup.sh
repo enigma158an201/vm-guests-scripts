8acb4e3b0f8904da1d333018a94204959521180e
enigma158an201 <gwenael.goujon@gmail.com>
2024-12-18 02:41:10 +0100

    add sys-backup.sh

---
+++ sys-backup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# script by enigma158an201
+set -euo pipefail # set -euxo pipefail
+
+sBackupHost=gwen@192.168.0.53
+getBackupFilename() {
+	if command -v hostname; then sHostName=$(hostname); else exit 1; fi
+	sBackupFile="ssh_backup_${sHostName}_$(date +%Y-%m-%d).tar.gz"
+	echo "${sBackupFile}"
+}
+sBackupFile="$(getBackupFilename)"
+
+main_sys_bakcup() {
+    if ! command -v tar  &>/dev/null || ! command -v ssh &>/dev/null; then
+        echo -e "\t>>> tar or ssh not found, exit now !!!"
+    fi
+    cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
+    tar -cvpz \
+    --exclude=/proc \
+    --exclude=/tmp \
+    --exclude=/mnt \
+    --exclude=/dev \
+    --exclude=/sys \
+    --exclude=/run \
+    --exclude=/media \
+	--exclude=/var/log \
+	--exclude=/var/cache \
+	--exclude=/usr/src/linux-headers* \
+	--exclude=/home/*/.gvfs \
+	--exclude=/home/*/.cache \
+	--exclude=/home/*/.local/share/Trash / | ssh "${sBackupHost}" "( cat > ssh_backup.tar.gz )"
+}
+
+main_sys_bakcup
