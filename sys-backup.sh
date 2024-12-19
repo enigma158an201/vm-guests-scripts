0a0ff9ecc31f99df8951b0a9b20b2d44458acf15
enigma158an201 <gwenael.goujon@gmail.com>
2024-12-19 07:36:01 +0100

    add chekroot to sys-backup.sh

--- sys-backup.sh
+++ sys-backup.sh
@@ -4,6 +4,13 @@
 set -euo pipefail # set -euxo pipefail
 
 sBackupHost=gwen@192.168.0.53
+checkRootPermissions() {
+	if [[ ${UID} = 0 ]] || [[ ${EUID} = 0 ]]; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
 getBackupFilename() {
 	if command -v hostname &>/dev/null; then sHostName=$(hostname); else exit 1; fi
 	sBackupFile="ssh_backup_${sHostName}_$(date +%Y-%m-%d).tar.gz"
@@ -12,25 +19,30 @@ getBackupFilename() {
 sBackupFile="$(getBackupFilename)"
 
 main_sys_bakcup() {
-    if ! command -v tar &>/dev/null || ! command -v ssh &>/dev/null; then
-        echo -e "\t>>> tar or ssh not found, exit now !!!"
-    fi
-    cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
+	if ! command -v tar &>/dev/null || ! command -v ssh &>/dev/null; then
+		echo -e "\t>>> tar or ssh not found, exit now !!!"
+		exit 1
+	fi
+	if [[ "$(checkRootPermissions)" = "false" ]]; then
+		echo -e "\t>>> root privileges are required, try with su | sudo | doas, exit now !!!"
+		exit 1
+	fi
+	cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
 	#shellcheck disable=SC2029
-    tar -cvpz \
-    --exclude=/proc \
-    --exclude=/tmp \
-    --exclude=/mnt \
-    --exclude=/dev \
-    --exclude=/sys \
-    --exclude=/run \
-    --exclude=/media \
-	--exclude=/var/log \
-	--exclude=/var/cache \
-	--exclude=/usr/src/linux-headers* \
-	--exclude=/home/*/.gvfs \
-	--exclude=/home/*/.cache \
-	--exclude=/home/*/.local/share/Trash / | ssh "${sBackupHost}" "( cat > \"${sBackupFile}\" )"
+	tar -cvpz \
+		--exclude=/proc \
+		--exclude=/tmp \
+		--exclude=/mnt \
+		--exclude=/dev \
+		--exclude=/sys \
+		--exclude=/run \
+		--exclude=/media \
+		--exclude=/var/log \
+		--exclude=/var/cache \
+		--exclude=/usr/src/linux-headers* \
+		--exclude=/home/*/.gvfs \
+		--exclude=/home/*/.cache \
+		--exclude=/home/*/.local/share/Trash / | ssh "${sBackupHost}" "( cat > \"${sBackupFile}\" )"
 }
 
 main_sys_bakcup
