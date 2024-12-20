3b3c79c226be0606df3b8ac27974a4af11636ed2
enigma158an201 <gwenael.goujon@gmail.com>
2024-12-20 13:48:54 +0100

    fix dnf upgrade & add git refresh

--- rocky-upgrade.sh
+++ rocky-upgrade.sh
@@ -14,13 +14,23 @@ REPOS_PKG="rocky-repos-${ROCKY_VERSION}.el9.noarch.rpm"
 GPG_KEYS_PKG="rocky-gpg-keys-${ROCKY_VERSION}.el9.noarch.rpm"
 
 update_dnf() {
-	if command -v sudo &>/dev/null; then 	sudo dnf update && sudo dnf full-upgrade
-	else 									dnf update && sudo dnf full-upgrade
+	if command -v sudo &>/dev/null; then 	sudo dnf update && sudo dnf upgrade
+	else 									dnf update && sudo dnf upgrade
 	fi
 }
 clean_dnf() {
 	if command -v sudo &>/dev/null; then 	sudo dnf autoremove && sudo dnf clean
 	else 									dnf autoremove && sudo dnf clean
+	fi
+}
+updateScriptsViaGit(){
+	set +euo pipefail #in case find cannot access some files or folders
+	sTargetScript="$(find ~ -nowarn -type f -iname git-pull-refresh.sh 2>/dev/null)" # -exec {} \;
+	set -euo pipefail
+	if test -f "${sTargetScript}"; then 
+		sGitFolder="$(dirname "${sTargetScript}")"
+		cd "${sGitFolder}" || exit 1
+		bash -x "${sTargetScript}"
 	fi
 }
 main_rockylinux_upgrade() {
@@ -30,6 +40,7 @@ main_rockylinux_upgrade() {
 	else
 		echo -e "\t>>> dnf found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4.shutdown vm"
 	fi
+	updateScriptsViaGit
 	update_dnf && clean_dnf #&& shutdown 0
 	if [[ ${sCurrentVersion} -lt "9" ]]; then 
 		sudo dnf install ${REPO_URL}/${RELEASE_PKG} ${REPO_URL}/${REPOS_PKG} ${REPO_URL}/${GPG_KEYS_PKG}
