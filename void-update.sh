ec4ab227aacda3d3ed744a5b46bd08e42c8b767f
enigma158an201 <gwenael.goujon@gmail.com>
2025-01-04 15:38:04 +0100

    add checkVirtEnv

--- void-update.sh
+++ void-update.sh
@@ -3,6 +3,17 @@
 # script by enigma158an201
 set -euo pipefail # set -euxo pipefail 
 
+checkVirtEnv() {
+	bFoundString=1 #false
+	if command -v sudo &>/dev/null; then 			sResult="$(sudo dmesg --notime)"
+	else 											sResult="$(dmesg --notime)"; fi
+	for sVirtEnv in virtualbox vboxservice vmware; do
+		if [[ ${sResult,,} =~ ${sVirtEnv} ]]; then 	bFoundString=0 #${bFoundString} || echo "true")" #="$(echo  | grep -i )"
+		#else 										bFoundString="$(${bFoundString} || echo "false")"
+		fi
+	done
+	echo "${bFoundString}"
+}
 update_void() {
 	sudo xbps-install -Su
 }
@@ -16,7 +27,9 @@ main_void_update() {
 	else
 		echo -e "\t>>> xbps found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4.shutdown vm"
 	fi
-	update_void && poweroff # && clean_void && shutdown 0
+	update_void #&& poweroff # && clean_void && shutdown 0
+	bVirtualized="$(checkVirtEnv)" #; echo "${bVirtualized}" 
+	if [[ ${bVirtualized} -eq 0 ]]; then poweroff; fi
 }
 
 main_void_update
