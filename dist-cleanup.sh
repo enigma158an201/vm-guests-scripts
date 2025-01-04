0a4093e11ac64455b098e3c2e41e9fc4eccd6b34
enigma158an201 <gwenael.goujon@gmail.com>
2025-01-04 14:19:54 +0100

    increase verbosity

--- dist-cleanup.sh
+++ dist-cleanup.sh
@@ -13,6 +13,7 @@ cachesDirectoryClean() { #find ~/.cache/ -type f -atime +365 -delete #rm -rfv ~/
 		#find ${sFolder} -type f -iwholename "*cache/*" -mtime +365 -delete # use mtime if noatime enabled, else atime
 	#done
 	#find / -type d \( -name "cache" -o -name ".cache" \) 2>/dev/null
+	echo -e "\t>>> cleaning old system cache files, if applicable"
 	for sFolder in /home/* /root; do #		for sSubFolder in .cache .cpan; do #${sSubFolder}
 		if test -d "${sFolder}/.cache/"; then 		find "${sFolder}/.cache/" -type f -mtime +365 -delete	# use mtime if noatime enabled, else atime
 													find "${sFolder}/.cache/" -type d -empty -delete
@@ -26,6 +27,7 @@ cachesDirectoryClean() { #find ~/.cache/ -type f -atime +365 -delete #rm -rfv ~/
 	done
 }
 cpanDirectoryClean() {
+	echo -e "\t>>> cleaning old cpan builds, if applicable"
 	for sFolder in /home/* /root; do
 		if test -d "${sFolder}/.cpan/build/"; then 	find "${sFolder}/.cpan/build/" -maxdepth 1 -type d -mtime +365  -exec rm -r {} \; #-delete
 													find "${sFolder}/.cpan/build/" -type d -empty -delete
@@ -33,45 +35,49 @@ cpanDirectoryClean() {
 	done
 }
 aptRemoveUnused() {
+	echo -e "\t>>> cleaning and purging unused apt packages, if applicable"
 	apt-get autoremove --purge
 	apt-get purge ~c
 	if apt-get distclean; then echo ""; else apt-get autoclean; fi
 }
 aptRemoveForeignFonts() {
-	if command -v apt-get &>/dev/null; then 	apt-get remove "fonts-noto*"
-												apt-get install fonts-noto-mono fonts-noto-unhinted fonts-noto-color-emoji
+	echo -e "\t>>> cleaning unused foreign, if applicable"
+	if command -v apt-get &>/dev/null; then 		apt-get remove "fonts-noto*"
+													apt-get install fonts-noto-mono fonts-noto-unhinted fonts-noto-color-emoji
 	fi
-	if command -v apt-get &>/dev/null; then 	dpkg-reconfigure fontconfig; fi
+	if command -v apt-get &>/dev/null; then 		dpkg-reconfigure fontconfig; fi
 }
 pacmanRemoveUnused() {
+	echo -e "\t>>> cleaning unused pacman packages, if applicable"
 	#shellcheck disable=SC2046
-	while pacman -Qdtq &>/dev/null; do
-		pacman -Rs $(pacman -Qdtq)
-		pacman -Scc
+	while pacman -Qdtq &>/dev/null; do 				pacman -Rs $(pacman -Qdtq)
+													pacman -Scc
 	done
 }
 flatpakRemoveUnused() {
-	if command -v flatpak &>/dev/null; then 	flatpak uninstall --unused; fi
+	echo -e "\t>>> cleaning unused flatpak packages, if applicable"
+	if command -v flatpak &>/dev/null; then 		flatpak uninstall --unused; fi
 }
 lessSystemdLogs() {
-	if command -v journalctl &>/dev/null; then
-		journalctl --vacuum-size=40M
-		sed -i 's/#SystemMaxUse=/SystemMaxUse=100M/' /etc/systemd/journald.conf
-		sed -i 's/#SystemMaxFiles=100/SystemMaxFiles=7/g' /etc/systemd/journald.conf
-		journalctl --rotate
+	echo -e "\t>>> cleaning old systemd log files and apply new settings, if applicable"
+	if command -v journalctl &>/dev/null; then 		journalctl --vacuum-size=100M
+													sed -i 's/#SystemMaxUse=/SystemMaxUse=100M/' /etc/systemd/journald.conf
+													sed -i 's/#SystemMaxFiles=100/SystemMaxFiles=7/g' /etc/systemd/journald.conf
+													journalctl --rotate
 	fi
 }
 lessSyslogLogs() {
-	if command -v rsyslogd &>/dev/null; then
-		sed -i 's/rotate 7/rotate 1/g' /etc/logrotate.d/rsyslog
-		sed -i 's/rotate 4/rotate 1/g' /etc/logrotate.d/rsyslog
-		sed -i 's/weekly/daily/g' /etc/logrotate.d/rsyslog
-		sed -i 's/rotate 4/rotate 1/g' /etc/logrotate.conf
-		sed -i 's/weekly/daily/g' /etc/logrotate.conf
+	echo -e "\t>>> cleaning old rsyslogd log files and apply new settings, if applicable"
+	if command -v rsyslogd &>/dev/null; then 		sed -i 's/rotate 7/rotate 1/g' /etc/logrotate.d/rsyslog
+													sed -i 's/rotate 4/rotate 1/g' /etc/logrotate.d/rsyslog
+													sed -i 's/weekly/daily/g' /etc/logrotate.d/rsyslog
+													sed -i 's/rotate 4/rotate 1/g' /etc/logrotate.conf
+													sed -i 's/weekly/daily/g' /etc/logrotate.conf
 	fi
 }
 lessFirewallLogs() {
-    if command -v ufw &>/dev/null; then 	ufw logging low; fi #ufw logging off
+	echo -e "\t>>> cleaning old ufw log files and apply new settings, if applicable"
+    if command -v ufw &>/dev/null; then 			ufw logging low; fi #ufw logging off
 }
 
 mainCleanUp() {
