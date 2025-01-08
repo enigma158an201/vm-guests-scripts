9c2c721dd14213a30bb33bc97d2786e114fd2f56
enigma158an201 <gwenael.goujon@gmail.com>
2025-01-09 00:39:25 +0100

    fixed errors

--- alpine-setup-de.sh
+++ alpine-setup-de.sh
@@ -51,13 +51,13 @@ select_option() {
 }
 gpu_apk() {
 	sChoiceGpu=$(select_option xf86-video-amdgpu xf86-video-ati xf86-video-intel xf86-video-nouveau xf86-video-qxl xf86-video-vesa xf86-video-vmware)
-	if command -v sudo &>/dev/null; then 	sudo apk "${sChoiceGpu}"
+	if command -v sudo &>/dev/null; then 	sudo apk add "${sChoiceGpu}"
 	else 									apk add "${sChoiceGpu}"
 	fi
 }
 input_apk() {
 	sChoiceInput=$(select_option xf86-input-evdev xf86-input-libinput xf86-input-synaptics xf86-input-vmmouse xf86-input-wacom)
-	if command -v sudo &>/dev/null; then 	sudo apk "${sChoiceInput}"
+	if command -v sudo &>/dev/null; then 	sudo apk add "${sChoiceInput}"
 	else 									apk add "${sChoiceInput}"
 	fi
 }
