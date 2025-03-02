79483a0e71a14ad913f14afed72986cf7b53004c
enigma158an201 <gwenael.goujon@gmail.com>
2025-03-02 23:13:03 +0100

    create network-setup/disable-ip6.sh

---
+++ network-setup/disable-ip6.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail #; set -x
+
+sLaunchDir="$(dirname "$0")"
+if [[ "${sLaunchDir}" = "." ]]; then sLaunchDir="$(pwd)"; elif [[ "${sLaunchDir}" = "include" ]]; then eval sLaunchDir="$(pwd)"; fi; sLaunchDir="${sLaunchDir//include/}"
+source "${sLaunchDir}/include/check-user-privileges"
+
+#eval "${sSuPfx} shutdown 0"
+
+blacklist-ip6-kernel-modules-sysctl() {
+	sIp6BcklDst="/etc/sysctl.d/00-disable-ip6-R13.conf"
+	sIp6BcklSrc="${sLaunchDir}${sIp6BcklDst}"
+	if [[ ! -f "${sIp6BcklDst}" ]]; then
+		echo -e "\t>>> proceed add disable ipv6 file to /etc/sysctl.d/ "
+        #shellcheck disable=SC2154
+		eval "${sSuPfx} mkdir -p \"$(dirname "${sIp6BcklDst}")\""
+		eval "${sSuPfx} install -o root -g root -m 0744 -pv ${sIp6BcklSrc} ${sIp6BcklDst}"
+	fi
+	unset sIp6Bckl{Dst,Src}
+}
+applySysctl() {
+    if command -v update-initramfs &>/dev/null; then 	eval "${sSuPfx} update-initramfs -u -k all"
+    fi
+}
+main() {
+    blacklist-ip6-kernel-modules-sysctl
+}
+main
