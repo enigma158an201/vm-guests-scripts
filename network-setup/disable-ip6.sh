56692374ea2ccb4e816bf8f9f38ff661645cf404
enigma158an201 <gwenael.goujon@gmail.com>
2025-03-02 23:21:23 +0100

    fix include network-setup/disable-ip6.sh

--- network-setup/disable-ip6.sh
+++ network-setup/disable-ip6.sh
@@ -4,7 +4,7 @@ set -euo pipefail #; set -x
 
 #sLaunchDir="$(dirname "$0")"; if [[ "${sLaunchDir}" = "." ]]; then sLaunchDir="$(pwd)"; elif [[ "${sLaunchDir}" = "include" ]]; then eval sLaunchDir="$(pwd)"; fi; sLaunchDir="${sLaunchDir//include/}"
 sLaunchDir="$(readlink -f "$(dirname "$0")")"
-source "${sLaunchDir}/include/check-user-privileges"
+source "${sLaunchDir}/../include/check-user-privileges"
 
 blacklist-ip6-kernel-modules-sysctl() {
 	sIp6BcklDst="/etc/sysctl.d/00-disable-ip6-R13.conf"
