1390f98b17c2c26f308f77e5fed23905c59d23c9
enigma158an201 <gwenael.goujon@gmail.com>
2025-03-25 16:36:32 +0100

    update user privileges scripts

--- dist-upgrades/include/test-superuser-privileges.sh
+++ dist-upgrades/include/test-superuser-privileges.sh
@@ -12,9 +12,9 @@ declare bSudoersUser
 declare bDoasUser
 
 checkUserSudoOrWheelGroup() {
- 	#set +x #sUser=${USER}
+	#set +x #sUser=${USER}
 	sUserGroups="$(groups "${USER}")" 		# la commande id pourrait etre une alternative
-	sUserGroups="${sUserGroups##*: }"		#sUserGroups="${`groups ${USER}`##*: }"
+	sUserGroups="${sUserGroups##*: }"		# sUserGroups="${`groups ${USER}`##*: }"
 	bSudoGroup="false"
 	for sGr in sudo wheel; do
 		for sGr2 in ${sUserGroups}; do
@@ -29,22 +29,14 @@ checkUserSudoOrWheelGroup() {
 	#set +x #if false; then sudo -l -U ${USER}; fi
 	#if false; then
 		#printf "sPassword\n" | sudo -S /bin/chmod --help >/dev/null 2>&1
-		#if [[ $? -eq 0 ];then
-			#has_sudo_access="YES"
-		#else
-			#has_sudo_access="NO"
-		#fi
-
+		#if [[ $? -eq 0 ];then 		has_sudo_access="YES"
+		#else 						has_sudo_access="NO"; fi
 		#echo "Does user `id -Gn` has sudo access?: ${has_sudo_access}"
 	#fi
 	#if false; then
 		#`timeout -k 2 2 bash -c "sudo /bin/chmod --help" >&/dev/null 2>&1` >/dev/null 2>&1
-		#if [[ $? -eq 0 ];then
-		# has_sudo_access="YES"
-		#else
-		# has_sudo_access="NO"
-		#fi
-
+		#if [[ $? -eq 0 ];then 		has_sudo_access="YES"
+		#else 						has_sudo_access="NO"; fi
 		#echo "Does user `id -Gn` has sudo access?: ${has_sudo_access}"
 	#fi
 	#if false; then sudo --validate; fi
@@ -57,54 +49,50 @@ checkSudoers() {
 	bUserSudo="$(LANG=C sudo -v -A 2>&1)" #bUserSudo=$(LANG=C sudo -v -A || echo "false") # if is sudo error no SUDO_ASKPASS otherwise not
 	keyWord="try setting SUDO_ASKPASS"
 	if [[ ${bUserSudo} =~ ${keyWord} ]] || [[ "${bUserSudo}" = "" ]]; then 		bSudoers="true"
-	else 																		bSudoers="false"
-	fi
+	else 																		bSudoers="false"; fi
 	echo "${bSudoers}"
 } 
-checkDoasUser() {		#set +x
+checkDoasUser() {					#set +x
 	bUserDoas="$(LANG=C timeout -v 1 doas true 2>&1)" #echo "test doas valid user" 
 	keyWord="doas: Operation not permitted"
-	if [[ ! ${bUserDoas} =~ ${keyWord} ]]; then 			bDoasUser="true"
-	else 													bDoasUser="false"; fi
+	if [[ ! ${bUserDoas} =~ ${keyWord} ]]; then 								bDoasUser="true"
+	else 																		bDoasUser="false"; fi
 	echo "${bDoasUser}"
 }
-getSuCmd() {			#set +x
+getSuCmd() {						#set +x
 	if [[ ! "${sSudoPath}" = "false" ]] && { [[ ! "${bSudoGroup}" = "false" ]] || \
-		[[ ! "${bSudoersUser}" = "false" ]] ; }; then								sSuCmd="${sSudoPath}" #"/usr/bin/sudo"
+		[[ ! "${bSudoersUser}" = "false" ]] ; }; then							sSuCmd="${sSudoPath}" #"/usr/bin/sudo"
 	elif [[ ! "${sDoasPath}" = "false" ]] && [[ -f /etc/doas.conf ]] && \
-		[[ ! "${bSudoGroup}" = "false" ]]; then										sSuCmd="${sDoasPath}" #"/usr/bin/doas"
-	else																			sSuCmd="su -p -c"; fi #"su - -p -c"
+		[[ ! "${bSudoGroup}" = "false" ]]; then									sSuCmd="${sDoasPath}" #"/usr/bin/doas"
+	else																		sSuCmd="su -p -c"; fi #"su - -p -c"
 	echo "${sSuCmd}"
 }
 getSuCmdNoPreserveEnv() {			#set +x
 	if [[ ! "${sSudoPath}" = "false" ]] && { [[ ! "${bSudoGroup}" = "false" ]] || \
-		[[ ! "${bSudoersUser}" = "false" ]] ; }; then								sSuCmd="${sSudoPath}" #"/usr/bin/sudo"
+		[[ ! "${bSudoersUser}" = "false" ]] ; }; then							sSuCmd="${sSudoPath}" #"/usr/bin/sudo"
 	elif [[ ! "${sDoasPath}" = "false" ]] && [[ -f /etc/doas.conf ]] && \
-		[[ ! "${bSudoGroup}" = "false" ]]; then										sSuCmd="${sDoasPath}" #"/usr/bin/doas"
-	else																			sSuCmd="su - -c"; fi #"su - -p -c"
+		[[ ! "${bSudoGroup}" = "false" ]]; then									sSuCmd="${sDoasPath}" #"/usr/bin/doas"
+	else																		sSuCmd="su - -c"; fi #"su - -p -c"
 	echo "${sSuCmd}"
 }
 suExecCommand() {
 	sCommand="$*"
 	if [[ ! "${EUID}" = "0" ]]; then 					eval "${sPfxSu} ${sCommand}"
-	elif [[ "${EUID}" = "0" ]]; then 					eval "${sCommand}"
-	fi
+	elif [[ "${EUID}" = "0" ]]; then 					eval "${sCommand}"; fi
 }
 suExecCommandNoPreserveEnv() {
 	sCommand="$*"
 	if [[ ! "${EUID}" = "0" ]]; then 					eval "${sPfxSuNoEnv} ${sCommand}"
-	elif [[ "${EUID}" = "0" ]]; then 					eval "${sCommand}"
-	fi
+	elif [[ "${EUID}" = "0" ]]; then 					eval "${sCommand}"; fi
 }
 
 main_SU(){
-	sSudoPath="$(command -v sudo || echo "false")"
-	sDoasPath="$(command -v doas || echo "false")"
+	sSudoPath="$(command -v sudo &>/dev/null || echo "false")"
+	sDoasPath="$(command -v doas &>/dev/null || echo "false")"
 	bSudoGroup="$(checkUserSudoOrWheelGroup)"
 	bSudoersUser="$(checkSudoers)"
 	if [[ ! "${sDoasPath}" = "false" ]]; then 			bDoasUser="$(checkDoasUser)"
-	else 												bDoasUser="false"; fi
-	#suQuotes="$(getSuQuotes)"
+	else 												bDoasUser="false"; fi				#suQuotes="$(getSuQuotes)"
 	if ! sPfxSu="$(getSuCmd) "; then 					exit 01; fi
 	if ! sPfxSuNoEnv="$(getSuCmdNoPreserveEnv) "; then 	exit 01; fi
 	#tests
