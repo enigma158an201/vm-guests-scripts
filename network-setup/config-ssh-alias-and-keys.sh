d1546dd456b64f1bd447878d0e8c6e7f6db44b62
enigma158an201 <gwenael.goujon@gmail.com>
2025-03-26 15:07:43 +0100

    update network-setup/config-ssh-alias-and-keys.sh for ssh config import

--- network-setup/config-ssh-alias-and-keys.sh
+++ network-setup/config-ssh-alias-and-keys.sh
@@ -8,18 +8,18 @@ set -euo pipefail # set -euxo pipefail
 sLaunchDir="$(dirname "$0")"
 source "${sLaunchDir}/../include/check-user-privileges" #source "${sLaunchDir}/include/set-common-settings.sh"
 
-sSshSubFolder=.ssh
-sSshAliasConfig=${sSshSubFolder}/config
+sSshUserFolder=.ssh
+sSshAliasConfig=${sSshUserFolder}/config
 sSshAliasConfigd=${sSshAliasConfig}.d
-sSshAuthKeys=${sSshSubFolder}/authorized_keys
+sSshAuthKeys=${sSshUserFolder}/authorized_keys
 
-sSshRepoSource="${sLaunchDir}/home/user"
-sSshRepoConf=${sSshRepoSource}/${sSshSubFolder}
+sSshRepoSource="${sLaunchDir}/src/home/user"
+sSshRepoConf=${sSshRepoSource}/${sSshUserFolder}
 sSshRepoAliasConfig=${sSshRepoSource}/${sSshAliasConfig}
 sSshRepoAliasConfigd=${sSshRepoSource}/${sSshAliasConfigd}
 #sSshRepoAuthKeys=${sSshRepoSource}/${sSshAuthKeys}
 
-sSshLocalConf=${HOME}/${sSshSubFolder}
+sSshLocalConf=${HOME}/${sSshUserFolder}
 sSshLocalAliasConfig=${HOME}/${sSshAliasConfig}
 sSshLocalAliasConfigd=${HOME}/${sSshAliasConfigd}
 sSshLocalAuthKeys=${HOME}/${sSshAuthKeys}
@@ -59,24 +59,34 @@ importSshKeys() {
 	#done
 }
 updateSshdConfig() {
-	echo -e "\t>>> application des fichiers config sshd"
+	echo -e "\t>>> application des fichiers config ssh et sshd"
 	declare -a sConfList
 	#sConfList=( "$(find "${sLaunchDir}/etc/ssh/sshd_config.d/" -iname '*.conf')" ) #	sConfList=${sConfList//'\n'/' '}
 	#sConfList=( $(ls "${sLaunchDir}/etc/ssh/sshd_config.d/*.conf") )
 	mapfile -t sConfList < <(find "${sLaunchDir}/../src/etc/ssh/sshd_config.d/" -iname '*.conf')
 	export sConfList
-	suExecCommand "bash -x -c 'for sSshdConfigFile in ${sConfList[*]}; do
+	suExecCommand  "bash -x -c 'for sSshdConfigFile in ${sConfList[*]}; do
 		sSshdConfigFileName=\$(basename \"\${sSshdConfigFile}\")
 		sSshdConfigDst=/etc/ssh/sshd_config.d/\${sSshdConfigFileName}
 		sSshdConfigSrc=${sLaunchDir}\${sSshdConfigDst}
 		if [[ -d \$(dirname \"\${sSshdConfigDst}\") ]] && [[ -f \"\${sSshdConfigSrc}\" ]]; then
 			install -o root -g root -m 0744 -pv \${sSshdConfigSrc} \${sSshdConfigDst}
 		fi
-	done
-	for sSshCrypt in rsa dsa ecdsa; do
+	done"
+	mapfile -t sConfList < <(find "${sLaunchDir}/../src/etc/ssh/ssh_config.d/" -iname '*.conf')
+	export sConfList
+	suExecCommand "bash -x -c 'for sSshConfigFile in ${sConfList[*]}; do
+		sSshConfigFileName=\$(basename \"\${sSshConfigFile}\")
+		sSshConfigDst=/etc/ssh/ssh_config.d/\${sSshConfigFileName}
+		sSshConfigSrc=${sLaunchDir}\${sSshConfigDst}
+		if [[ -d \$(dirname \"\${sSshConfigDst}\") ]] && [[ -f \"\${sSshConfigSrc}\" ]]; then
+			install -o root -g root -m 0744 -pv \${sSshConfigSrc} \${sSshConfigDst}
+		fi
+	done"
+	suExecCommand "bash -x -c 'for sSshCrypt in rsa dsa ecdsa; do
 		rm /etc/ssh/ssh_host_*\$sSshCrypt*_key* || true
 	done
-	systemctl restart sshd.service'"
+	systemctl restart sshd.service ssh.service'"
 }
 cleanModuli() {
 	suExecCommand "awk '\$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe;
