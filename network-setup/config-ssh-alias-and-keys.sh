1abfd80a22d2abb2db49907c302a3c7be49e613c
enigma158an201 <gwenael.goujon@gmail.com>
2025-03-26 15:21:26 +0100

    fix network-setup/config-ssh-alias-and-keys.sh

--- network-setup/config-ssh-alias-and-keys.sh
+++ network-setup/config-ssh-alias-and-keys.sh
@@ -60,8 +60,7 @@ importSshKeys() {
 }
 updateSshdConfig() {
 	echo -e "\t>>> application des fichiers config ssh et sshd"
-	declare -a sConfList
-	
+	#declare -a sConfList
 	#mapfile -t sConfList < <(find "${sLaunchDir}/../src/etc/ssh/ssh_config.d/" -iname '*.conf')
 	#export sConfList
 	#suExecCommand "bash -x -c 'for sSshConfigFile in ${sConfList[*]}; do
@@ -72,17 +71,17 @@ updateSshdConfig() {
 	#		install -o root -g root -m 0744 -pv \"\${sSshConfigSrc}\" \"\${sSshConfigDst}\"
 	#	fi
 	#done'"
-	suExecCommand "rsync -av \"${sLaunchDir}/../src/etc/ssh/ssh_config.d/\" /etc/ssh/ssh_config/"
-	mapfile -t sConfList < <(find "${sLaunchDir}/../src/etc/ssh/sshd_config.d/" -iname '*.conf')
-	export sConfList
-	suExecCommand  "bash -x -c 'for sSshdConfigFile in ${sConfList[*]}; do
-		sSshdConfigFileName=\"\$(basename \"\${sSshdConfigFile}\")\"
-		sSshdConfigDst=\"/etc/ssh/sshd_config.d/\${sSshdConfigFileName}\"
-		sSshdConfigSrc=\"${sLaunchDir}\${sSshdConfigDst}\"
-		if [[ -d \"\$(dirname \"\${sSshdConfigDst}\")\" ]] && [[ -f \"\${sSshdConfigSrc}\" ]]; then
-			install -o root -g root -m 0744 -pv \"\${sSshdConfigSrc}\" \"\${sSshdConfigDst}\"
-		fi
-	done'"
+	suExecCommand "rsync -av \"${sLaunchDir}/../src/etc/ssh/sshd_config.d/\" /etc/ssh/sshd_config.d/; rsync -av \"${sLaunchDir}/../src/etc/ssh/ssh_config.d/\" /etc/ssh/ssh_config.d/"
+	#mapfile -t sConfList < <(find "${sLaunchDir}/../src/etc/ssh/sshd_config.d/" -iname '*.conf')
+	#export sConfList
+	#suExecCommand  "bash -x -c 'for sSshdConfigFile in ${sConfList[*]}; do
+	#	sSshdConfigFileName=\"\$(basename \"\${sSshdConfigFile}\")\"
+	#	sSshdConfigDst=\"/etc/ssh/sshd_config.d/\${sSshdConfigFileName}\"
+	#	sSshdConfigSrc=\"${sLaunchDir}\${sSshdConfigDst}\"
+	#	if [[ -d \"\$(dirname \"\${sSshdConfigDst}\")\" ]] && [[ -f \"\${sSshdConfigSrc}\" ]]; then
+	#		install -o root -g root -m 0744 -pv \"\${sSshdConfigSrc}\" \"\${sSshdConfigDst}\"
+	#	fi
+	#done'"
 	#suExecCommand "bash -x -c 'for sSshCrypt in rsa dsa ecdsa; do rm /etc/ssh/ssh_host_*\$sSshCrypt*_key* || true; done; systemctl restart sshd.service ssh.service'"
 }
 cleanModuli() {
