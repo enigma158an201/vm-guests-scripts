7d24482f5726d64ff34f6ca07957169c0f969a06
enigma158an201 <gwenael.goujon@gmail.com>
2025-09-13 02:02:43 +0200

    update dist-upgrades/pre-dist-upgrade-backup.sh

--- dist-upgrades/pre-dist-upgrade-backup.sh
+++ dist-upgrades/pre-dist-upgrade-backup.sh
@@ -25,9 +25,28 @@ getDiskBackupFolder() { 		if [[ ! -d /${sDistBackupFolder} ]]; then 		mkdir -p "
 backupDpkgSelections() { 		if command -v dpkg &>/dev/null; then 			dpkg --get-selections '*' > "/${sDpkgGetSelections}"; fi; }
 backupAptitudePkgstates() { 	if [[ -f /${sVarLibAptitudePkgstates} ]]; then 	tar -czf "/${sDistBackupFolder}/${sVarLibAptitudePkgstates//'/'/'-'}-backup.tar.gz" -C / ${sVarLibAptitudePkgstates}; fi; } #cp -a "${sVarLibAptitudePkgstates}" "${sDistBackupFolder}/"
 backupAptExtendedStates() { 	if [[ -f /${sVarLibAptExt} ]]; then 			tar -czf "/${sDistBackupFolder}/${sVarLibAptExt//'/'/'-'}-backup.tar.gz" -C / /${sVarLibAptExt}; fi; } #cp -a "${sVarLibAptExt}" "${sDistBackupFolder}/"
-backupEtcFolder() { 			if [[ -d /${sEtcFolder} ]]; then 				tar -czf "/${sDistBackupFolder}/${sEtcFolder}-backup.tar.gz" -C / etc; fi; }
+backupEtcFolder() { 			if [[ -d /${sEtcFolder} ]]; then 				tar -czf "/${sDistBackupFolder}/${sEtcFolder}-backup.tar.gz" -C / ${sEtcFolder}; fi; }
 backupVarLibDpkg() { 			if [[ -d /${sVarLibDpkg} ]]; then 				tar -czf "/${sDistBackupFolder}/${sVarLibDpkg//'/'/'-'}-backup.tar.gz" -C / ${sVarLibDpkg}; fi; }	
-
+upgradeCheck() {
+	apt-get autoremove
+	apt-mark showhold | grep -q . && { echo "There are held packages. Please unhold them before proceeding with the upgrade."; exit 1; } # apt-mark hold package_name || apt-mark unhold package_name
+	dpkg --audit | grep -q . && { echo "There are broken packages. Please fix them before proceeding with the upgrade."; exit 1; }
+	if apt-get list '?obsolete' 2>/dev/null | grep -q .; then
+		echo "There are obsolete packages. Please remove them before proceeding with the upgrade."
+		apt-get autoremove
+		exit 1
+	fi
+	if apt-get list '?orphaned' 2>/dev/null | grep -q .; then
+		echo "There are orphaned packages. Please remove them before proceeding with the upgrade."
+		apt-get autoremove
+		exit 1
+	fi
+	if apt-get list '?config-files' 2>/dev/null | grep -q .; then
+		echo "There are packages with only configuration files remaining. Please purge them before proceeding with the upgrade."
+		apt-get autoremove
+		exit 1
+	fi
+}
 main() {
 	getDiskBackupFolder
 	backupDpkgSelections
