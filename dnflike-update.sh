a0ffe19a5864ab51d63194ae6c5dfe1a87641a07
enigma158an201 <gwenael.goujon@gmail.com>
2026-06-27 18:15:34 +0200

    fix dnflike-update.sh

--- dnflike-update.sh
+++ dnflike-update.sh
@@ -11,26 +11,26 @@ source "${sLaunchDir}/include/check-user-privileges" || source "${sParentDir}/in
 source "${sLaunchDir}/include/check-virtual-env" || 	source "${sParentDir}/include/check-virtual-env"
 source "${sLaunchDir}/include/git-self-update" || 		source "${sParentDir}/include/git-self-update"
 
-update_dnf() {
+updatehDnf() {
 	#shellcheck disable=SC2154
-	if command -v "${sSuPfx}" &>/dev/null; then eval "${sSuPfx} 'dnf update && dnf upgrade'"
+	if command -v "${sSuPfx}" &>/dev/null; then eval "${sSuPfx} 'dnf update'" && eval "${sSuPfx} 'dnf upgrade'"
 	else 										dnf update && dnf upgrade
 	fi
 }
-clean_dnf() {
-	if command -v "${sSuPfx}" &>/dev/null; then eval "${sSuPfx} 'dnf autoremove && dnf clean all'"
+cleanDnf() {
+	if command -v "${sSuPfx}" &>/dev/null; then eval "${sSuPfx} 'dnf autoremove'" && eval "${sSuPfx} 'dnf clean all'"
 	else 										dnf autoremove && dnf clean all
 	fi
 }
-main_rockylinux_update() {
+mainRockylinuxUpdate() {
 	if ! command -v dnf &>/dev/null; then 		echo -e "\t--> dnf not found, exit now !!!"
 												exit 1
 	else 										echo -e "\t--> dnf found, this script will:\n 1. fetch updates\n 2. install updates\n 3. clean pkg archives\n 4. shutdown vm"
 	fi
 	updateScriptsViaGit
-	update_dnf && clean_dnf #&& shutdown 0
+	updateDnf && cleanDnf #&& shutdown 0
 	bVirtualized="$(checkVirtEnv)" #; echo "${bVirtualized}" 
 	if [[ ${bVirtualized} -eq 0 ]]; then 		eval "${sSuPfx} shutdown 0"; fi
 }
 
-main_rockylinux_update
+mainRockylinuxUpdate
